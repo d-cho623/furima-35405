a9606073d4e7775cd860dc7dfb3c74a8ce1d63ed
d-cho623 <s3.ikeike344@gmail.com>
2021-04-20 18:28:21 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_delivery_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
+  validates :image, presence: true
+
   belongs_to :user
+  has_one_attached :image
   # has_one :buy_info
 end
