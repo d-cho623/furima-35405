345b5ffbfe15e799478cba9907c17510d9fc4785
d-cho623 <s3.ikeike344@gmail.com>
2021-04-21 10:57:55 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,22 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, presence: true
-  validates :sales_status_id, presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :scheduled_delivery_id, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :sales_status_id, numericality: { other_than: 1 } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 } 
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
   validates :image, presence: true
 
   belongs_to :user
   has_one_attached :image
   # has_one :buy_info
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 end
