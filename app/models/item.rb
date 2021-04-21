9c16b039c3ef2e450192a4fe7183fa0f7422e447
d-cho623 <s3.ikeike344@gmail.com>
2021-04-21 11:16:26 +0900

    ActiveHashの記述の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  belongs_to :user
+  has_one_attached :image
+  # has_one :buy_info
+
   validates :name, presence: true
   validates :info, presence: true
   validates :category_id, numericality: { other_than: 1 } 
@@ -9,14 +21,4 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
   validates :image, presence: true
 
-  belongs_to :user
-  has_one_attached :image
-  # has_one :buy_info
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :sales_status
-  belongs_to :shipping_fee_status
-  belongs_to :prefecture
-  belongs_to :scheduled_delivery
 end
