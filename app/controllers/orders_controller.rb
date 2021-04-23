325d498072ec9d46133180c03fc6ea5ada058611
d-cho623 <s3.ikeike344@gmail.com>
2021-04-23 16:40:36 +0900

    ユーザーと購入済みアイテム関係のコントローラーとビューファイルの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if @item.order.present?
   end
 
   def create
