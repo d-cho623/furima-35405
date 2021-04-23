94ada90d550282042fba15393785c1facc281868
d-cho623 <s3.ikeike344@gmail.com>
2021-04-23 19:14:41 +0900

    テストコード内のカラム名を修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_address do
     postal_code { '123-4567' }
-    prefecture { 2 }
+    prefecture_id { 2 }
     city { '東京都' }
     house_number { '1-1' }
     building { '東京ハイツ' }
