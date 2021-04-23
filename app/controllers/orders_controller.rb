6501f8416e355f5e632fa92fc6b2f093bf473a1a
d-cho623 <s3.ikeike344@gmail.com>
2021-04-23 11:39:16 +0900

    商品購入ページの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,23 @@
 class OrdersController < ApplicationController
-
   def index
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    @order_address.save
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
-  
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+    params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id]
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,20 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :house_number
-    validates :phone_number, format: {with: /\A[0-9]{,11}}\z/ }
+    validates :phone_number, format: { with: /\A[0-9]{,11}$\z/ }
     validates :user_id
     validates :item_id
   end
-  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture, numericality: { other_than: 1 }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order_id)
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building: building,
+                   phone_number: phone_number, order_id: order.id)
   end
 end
