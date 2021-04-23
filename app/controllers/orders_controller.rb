684dabdccdc866f621670dcf4ac41516fc873ece
d-cho623 <s3.ikeike344@gmail.com>
2021-04-23 15:28:07 +0900

    PAYJPを用いたクレジットカードでの決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item 
       @order_address.save
       redirect_to root_path
     else
@@ -15,9 +16,19 @@ class OrdersController < ApplicationController
     end
   end
 
+  private
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture, :city, :house_number, :building, :phone_number).merge(
-      user_id: current_user.id, item_id: params[:item_id]
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
 end
